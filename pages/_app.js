f9a3a7518cd24f07fec81fb8f855672476443cff
Jamie Howells <jamie.howells@gmail.com>
2022-10-11 14:38:48 +0100

    Base CSS and scaffold for app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import '../styles/app.css';
+import Link from 'next/link';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const Marketplace = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <nav className='border-b p-6' style={{ background: '#03045E' }}>
+        <p className='text-4x1 font-bold text-white'>Moonbirdz Marketplace</p>
+        <div className='flex mt-4 justify-center'>
+          <Link href='/'>
+            <a className='mr-4'>
+              Main Marketplace
+            </a>
+          </Link>
+          <Link href='/mint-tokens'>
+            <a className='mr-4'>
+              Mint Tokens
+            </a>
+          </Link>
+          <Link href='/my-nfts'>
+            <a className='mr-4'>
+              My NFTs
+            </a>
+          </Link>
+          <Link href='/account-dashboard'>
+            <a className='mr-4'>
+              Account Dashboard
+            </a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  );
+};
 
-export default MyApp
+export default Marketplace;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{html,js}',
+  ],
   theme: {
     extend: {},
   },
