a6eb7d135e53dc6142f34fcc32b2cd2466af0240
Jamie Howells <jamie.howells@gmail.com>
2022-10-14 13:39:49 +0100

    NFT loading for index page

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import '../styles/globals.css';
 import '../styles/app.css';
+import Head from 'next/head';
 import Link from 'next/link';
 
 const Marketplace = ({ Component, pageProps }) => {
   return (
     <div>
+      <Head>
+        <title>Moonbirdz Marketplace</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <nav className='border-b p-6' style={{ background: '#03045E' }}>
         <p className='text-4x1 font-bold text-white'>Moonbirdz Marketplace</p>
         <div className='flex mt-4 justify-center'>
